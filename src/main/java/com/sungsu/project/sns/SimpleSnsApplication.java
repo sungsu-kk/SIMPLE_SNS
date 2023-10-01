51b456ef9fe4f339e81c64d58768efe0de9d8d6a
김성수(Kay) <108313829+sungsu-kk@users.noreply.github.com>
2023-10-01 15:19:07 +0900

    init:init project

---
+++ src/main/java/com/sungsu/project/sns/SimpleSnsApplication.java
@@ -0,0 +1,14 @@
+package com.sungsu.project.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SimpleSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSnsApplication.class, args);
+    }
+
+}
